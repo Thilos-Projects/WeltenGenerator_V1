718a1a5eda39cd0020cdd1bed3a59c43ee5490e3
Thilos-Projects <50086979+Thilos-Projects@users.noreply.github.com>
2025-02-15 14:14:41 +0100

    File and folder system is good
    Signed-off-by: Thilos-Projects <50086979+Thilos-Projects@users.noreply.github.com>

--- V1/CreateWorld/CreateWorld.cpp
+++ V1/CreateWorld/CreateWorld.cpp
@@ -13,7 +13,25 @@ uint8_t main(int argc, char* argv[]) {
 		worldPath = argv[1];
 
 	File_Manager::setWorldPath(worldPath);
-	File_Manager::createFolder("HI");
+	std::cout << File_Manager::createFolder("HI") << std::endl;
+	std::cout << File_Manager::createFolder("HI") << std::endl;
+	std::cout << File_Manager::folderExists("HI") << std::endl;
+	File_Manager::removeFolder("HI");
+	std::cout << File_Manager::folderExists("HI") << std::endl;
+
+	std::cout << File_Manager::createFile("HI", "test.txt") << std::endl;
+	std::cout << File_Manager::folderExists("HI") << std::endl;
+	std::cout << File_Manager::fileExists("HI", "test.txt") << std::endl;
+	File_Manager::removeFolder("HI");
+	std::cout << File_Manager::folderExists("HI") << std::endl;
+	std::cout << File_Manager::fileExists("HI", "test.txt") << std::endl;
+
+	std::cout << File_Manager::createFile("HI", "test.txt") << std::endl;
+	std::cout << File_Manager::folderExists("HI") << std::endl;
+	std::cout << File_Manager::fileExists("HI", "test.txt") << std::endl;
+	File_Manager::removeFile("HI", "test.txt");
+	std::cout << File_Manager::folderExists("HI") << std::endl;
+	std::cout << File_Manager::fileExists("HI", "test.txt") << std::endl;
 
 	//uint8_t temp;
 	//std::cin >> temp;
--- V1/CreateWorld/src/FileManager.cpp
+++ V1/CreateWorld/src/FileManager.cpp
@@ -6,32 +6,20 @@ namespace fs = std::filesystem;
 namespace File_Manager {
 	std::string worldPath;
 
-
 	void setWorldPath(std::string path) {
 		worldPath = path + "/";
 	}
 
 	void removePath(std::string path) {
-			fs::remove(worldPath + path);
+		fs::remove_all(worldPath + path);
 	}
-
 	bool pathExists(std::string path) {
 		return fs::exists(worldPath + path);
-	}
-
-	bool pathExists(std::string path) {
-		return fs::exists(worldPath + path);
-	}
-
-	bool fileExists(std::string path) {
-		return pathExists(path);
 	}
 
 	bool folderExists(std::string path) {
 		return pathExists(path);
 	}
-
-	
 	bool createFolder(std::string path) {
 		return fs::create_directories(worldPath + path);
 	}
@@ -39,13 +27,18 @@ namespace File_Manager {
 		removePath(path);
 	}
 
-	void createFile(std::string parrentFolder, std::string name) {
+	bool createFile(std::string parrentFolder, std::string name) {
 		createFolder(parrentFolder);
 		std::ofstream ofs(worldPath + parrentFolder + "/" + name);
+		bool isOpen = ofs.is_open();
 		ofs.close();
+		return isOpen;
 	}
 	void removeFile(std::string parrentFolder, std::string name) {
 		removePath(parrentFolder + "/" + name);
+	}
+	bool fileExists(std::string parrentFolder, std::string name) {
+		return pathExists(parrentFolder + "/" + name);
 	}
 
 }
